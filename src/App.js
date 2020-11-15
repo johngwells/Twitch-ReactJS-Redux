83cd90df3608035720c1e6d6fedd11c6cc3fd57b
John Wells <molekewl2017@gmail.com>
2020-11-14 20:42:57 -0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div><Link to='/pagetwo'>PageTwo</Link></div>
+}
+
+const PageTwo = () => {
+  return <div><Link to='/'>PageOne</Link></div>
+}
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={PageOne} />
+          <Route path='/pagetwo' exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
