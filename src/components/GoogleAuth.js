c7eb4fc3100c59e363adb61397250ae3af21168b
John Wells <molekewl2017@gmail.com>
2020-11-18 23:13:43 -0800

    rename function names | add redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -27,11 +27,11 @@ class GoogleAuth extends React.Component {
     });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   }
 
@@ -40,14 +40,14 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className='ui red google button'>
+        <button onClick={this.onSignOutClick} className='ui red google button'>
           <i className='google icon' />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className='ui blue google button'>
+        <button onClick={this.onSignInClick} className='ui blue google button'>
           <i className='google icon' />
           Sign In
         </button>
