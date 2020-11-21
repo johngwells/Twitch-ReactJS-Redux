624b5a3c3de5c2254255729288aa026256a46c9a
John Wells <molekewl2017@gmail.com>
2020-11-20 23:54:38 -0800

    clean up using ternary op

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -24,11 +24,7 @@ class GoogleAuth extends React.Component {
   }
 
   onAuthChange = (isSignedIn) => {
-    if (isSignedIn) {
-      this.props.signIn();
-    } else {
-      this.props.signOut();
-    }
+    isSignedIn ? this.props.signIn() : this.props.signOut()
   };
 
   onSignInClick = () => {
