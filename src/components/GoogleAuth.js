86037c5278189dc12bb19c82e5d560e169dcb122
John Wells <molekewl2017@gmail.com>
2020-11-21 00:32:36 -0800

    refactor

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
-
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       // init creates a promise
@@ -17,28 +15,37 @@ class GoogleAuth extends React.Component {
         .then(() => {
           // create a auth instance
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          // Before redux
+          // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+          // with redux
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
   onAuthChange = (isSignedIn) => {
-    isSignedIn ? this.props.signIn() : this.props.signOut()
+    if (isSignedIn) {
+      this.props.signIn() 
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
     this.auth.signIn();
-  }
+  };
 
   onSignOutClick = () => {
     this.auth.signOut();
-  }
+  };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className='ui red google button'>
           <i className='google icon' />
@@ -60,4 +67,8 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default connect(null, { signIn, signOut })(GoogleAuth);
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,18 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 const INITIAL_STATE = {
   isSignedIn: null
 };
 
-export default (state = INITIAL_STATE, action) => {
+const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SIGN_IN':
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case 'SIGN_OUT':
-      return { ...state, isSignedin: false };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false };
     default:
       return state;
   }
 };
+
+export default authReducer;
