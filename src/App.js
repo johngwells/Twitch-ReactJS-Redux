fbae92e8faa6efb75b3cf6f0edfd295d35baed12
John Wells <molekewl2017@gmail.com>
2020-12-03 08:44:47 -0800

    create history object component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import StreamCreate from './components/streams/StreamCreate';
 import StreamEdit from './components/streams/StreamEdit';
 import StreamDelete from './components/streams/StreamDelete';
 import StreamList from './components/streams/StreamList';
 import StreamShow from './components/streams/StreamShow';
+import history from './history';
 
 import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path='/' exact component={StreamList} />
@@ -21,7 +22,7 @@ const App = () => {
           <Route path='/streams/delete' exact component={StreamDelete} />
           <Route path='/streams/show' exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
